ea4a68cca5cb46c88dc095396d482d3c6ce0110a
madisonwheeler <madisonawheeler@gmail.com>
2017-11-01 21:07:32 -0400

    Updated Property Schema to require Appliance and Rent Schemas

--- app/models/Property.js
+++ app/models/Property.js
@@ -1,19 +1,22 @@
 var mongoose = require('mongoose');
+var user = require('./Users.js');
+var rent = require('./Rent.js');
+var appliance = require('./Appliance.js');
 
 var propertySchema = new mongoose.Schema({
     name: String,
     address: String,
     users: [{
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User'
+        ref: user
     }],
     rent_payments: [{
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Rent'
+        ref: rent
     }], 
     appliances: [{
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Appliance'
+        ref: appliance
     }]
 });
 
