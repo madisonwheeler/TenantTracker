97a12085361e72d603575f45bdcb21ed4f63292e
Madison Wheeler <madisonawheeler@gmail.com>
2017-11-27 14:26:06 -0500

    Started Mocha tests

--- test/unit/user.test.js
+++ test/unit/user.test.js
@@ -1,73 +1,80 @@
 var db = require('../../app/models');
 var User = db.User;
 
-// describe('user', function () {
-//   it('should load', function (done) {
-//     User.create({
-//       id: 5, 
-//       username: 'test123', 
-//       password: 'strongpass', 
-//       name: 'bob', 
-//       phone_number: '11111111111', 
-//       email: 'test@test', 
-//       user_type: 'landlord', 
-//       landlord_id: 33, 
-//       property_id: 21
-//     }).then( function (article) {
-//       // do some tests on article here
-//       done();
-//     });
-//   });
-// });  
+'use strict';
+var expect = require('expect.js');
 
-
-//   module.exports = function (sequelize, DataTypes) {
-
-//   var User = sequelize.define('User', {
-//     id: DataTypes.INTEGER, 
-//     username: DataTypes.STRING, 
-//     password: DataTypes.STRING, 
-//     name: DataTypes.STRING, 
-//     phone_number: DataTypes.STRING, 
-//     email: DataTypes.STRING, 
-//     user_type: DataTypes.STRING, 
-//     landlord_id: DataTypes.INTEGER, 
-//     property_id: DataTypes.INTEGER
-//   }, {
-//     classMethods: {
-//       associate: function (models) {
-//         // example on how to add relations
-//         // Article.hasMany(models.Comments);
-//       }
-//     }
-//   });
-
-//   return User;
-// };
-
-// 'use strict';
-
- var expect = require('expect.js');
-
- describe('models/User', function () {
+describe('models/User', function () {
     before(function () {
         return require('../../app/models').sequelize.sync();
     });
 
-     beforeEach(function () {
+    beforeEach(function () {
        this.User = require('../../app/models').User;
-       this.Rent = require('../../app/models').Rent;
-     });
+    });
 
-     describe('create', function () {
-       it('creates a task', function () {
-//       	this.timeout(5000);
-//   		setTimeout(done, 300);
-//   //      return this.User.create({ id: 5, username: 'test123', password: 'strongpass', name: 'bob', phone_number: '11111111111', email: 'test@test', user_type: 'landlord', landlord_id: 33, property_id: 21 }).bind(this).then(function (user) {
-//   // //       return this.Task.create({ title: 'a title', UserId: user.id }).then(function (rent) {
-//   //   //       expect(rent.title).to.equal('a title');
-//   //     //   });
-//   //      });
+    describe('create', function () {
+       	it('Tests creation of a User', function () {
        });
-     });
- });
+    });
+});
+
+describe('models/Rent', function () {
+    before(function () {
+        return require('../../app/models').sequelize.sync();
+    });
+
+    beforeEach(function () {
+       this.Rent = require('../../app/models').Rent;
+    });
+
+    describe('create', function () {
+       	it('Tests creation of a Rent Payment', function () {
+       });
+    });
+});
+
+describe('models/Appliance', function () {
+    before(function () {
+        return require('../../app/models').sequelize.sync();
+    });
+
+    beforeEach(function () {
+       this.User = require('../../app/models').User;
+    });
+
+    describe('create', function () {
+       	it('Tests creation of an Appliance', function () {
+       });
+    });
+});
+
+describe('models/Property', function () {
+    before(function () {
+        return require('../../app/models').sequelize.sync();
+    });
+
+    beforeEach(function () {
+       this.User = require('../../app/models').User;
+    });
+
+    describe('create', function () {
+       	it('Tests creation of a Property', function () {
+       });
+    });
+});
+
+describe('models/index', function () {
+    before(function () {
+        return require('../../app/models').sequelize.sync();
+    });
+
+    beforeEach(function () {
+       this.Index = require('../../app/models').Index;
+    });
+
+    describe('create', function () {
+       	it('Tests creation of index', function () {
+       });
+    });
+});
