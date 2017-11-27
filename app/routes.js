6ca69c3577ad37a5565b396da6929ed8eddbb1db
Max Wang <wangm13@rpi.edu>
2017-11-27 02:55:40 -0500

    added routes for landlord homepage. started adding routes for appliances

--- app/routes.js
+++ app/routes.js
@@ -35,39 +35,84 @@ module.exports = function(app) {
 	});
 
 
-	//update Appliance status
-    app.get('/api/properties/update', function(req, res) {
-		console.log("here1");
-		console.log(req.query.repairDesc);
-		models.Appliance.findOne({
-			where: {id: parseInt(req.query.userID)}
-		}).then(function(employee) {
-			console.log(employee);
-			if(employee == null) {
+	//get all appliance for that property
+    app.get('/api/appliance', function(req, res) {
+		models.Appliance.findAll({
+			where: {property_id: parseInt(req.query.property_id)}
+		}).then(function(appliances) {
+			console.log(appliances);
+			if(appliances == null) {
 				res.send("Not found");
 			}
 			else {
-				res.send(employee.dataValues);
+				res.send(appliances);
 			}
+		});
+
+    });
+
+	//update Appliance status
+    app.get('/api/appliance/update', function(req, res) {
+		console.log("here1");
+		console.log(req.query.repairDesc);
+		models.Appliance.findOne({
+			where: {id: parseInt(req.query.appliance_id)}
+		}).then(function(appliance) {
+			appliance.repairDesc = req.query.repairDesc;
+			appliance.status = "Needs Repair";
+			appliance.save();
 		});
 
 
     });
 
     //update Appliance status
-    app.get('/api/properties/fix', function(req, res) {
-		models.Unit.update({code: req.query.app_code} , { $set: {"repairDesc": "", "appliance1_status": "Good" } }, {upsert: true}).exec(function(err,units) {
-			if (err) {
-	        	console.log(err);
-			}
-			else {
-				res.send("Hello");
-			}
+    app.get('/api/appliance/fix', function(req, res) {
+		models.Appliance.findOne({
+			where: {id: parseInt(req.query.appliance_id)}
+		}).then(function(appliance) {
+			appliance.repairDesc = "";
+			appliance.status = "Good";
+			appliance.save();
 
 		});
 
 
     });
+
+
+
+    //landlord page routes
+
+
+    //get property
+    app.post('/api/landlord/property',function(req, res) {
+    	models.Property.findOne({where: {landlord_id: req.body.landlord_id} }).then(function(property) {
+    		if(property != null) {
+    			res.send(property);
+    		}
+    	});
+    });
+
+    //get property
+    app.post('/api/landlord/tenants',function(req, res) {
+    	models.User.findAll({where: {landlord_id: req.body.landlord_id} }).then(function(tenants) {
+    		if(tenants != null) {
+    			res.send(tenants);
+    		}
+    	});
+    });
+
+    //get property
+    app.post('/api/landlord/applianceCount',function(req, res) {
+    	models.Appliance.findAll({where: {property_id: req.body.property_id} }).then(function(appliances) {
+    		if(appliances != null) {
+    			res.send(appliances.length);
+    		}
+    	});
+    });
+
+
 	/*app.get('/api/todos', function(req, res) {
 
 		Todo.find(function(err, todos) {
